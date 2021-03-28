8d720688d8c801c364360c994abcf27a8cf6212e
Konstantin Dunaev <shop.overled@gmail.com>
2021-03-28 23:58:55 +0300

    Изменил рост

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
         BmiService service = new BmiService();
-        int height = 178;
+        int height = 180;
         int weight = 65;
         double bmi = service.calculate(height, weight);
         System.out.println("Your body mass index = " + bmi);
